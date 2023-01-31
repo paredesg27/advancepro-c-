2228cca809f25cf10a8b41f880b6459aba146d02
George Paredes <119899081+paredesg27@users.noreply.github.com>
2023-01-30 20:37:18 -0500

    Update main.cpp

--- assignment1/main.cpp
+++ assignment1/main.cpp
@@ -3,14 +3,11 @@
 using namespace std;
 
 vector<string> returnLongestString(vector<string> vectorArray){
-    int numOfLetters = 0;
+    int numOfLetters = vectorArray.at(0).length();
     vector<string> longestStrings;
+
     for(int i = 0; i < vectorArray.size(); i++){
-        if (i == 0){
-            numOfLetters = vectorArray.at(i).length();
-            longestStrings.push_back(vectorArray.at(i));
-        }
-        else if(vectorArray.at(i).length() >= numOfLetters){
+        if(vectorArray.at(i).length() >= numOfLetters){
             if (vectorArray.at(i).length() > numOfLetters){
                 numOfLetters = vectorArray.at(i).length();
                 longestStrings.clear();
@@ -28,26 +25,21 @@ int main() {
     vector<string> userInputtedStrings;
     string inputString;
     vector<string> longestStringArray;
-    cout << "Enter any number of String. Enter \"done\" to stop" << endl;
-    while( inputString != "done"){
-        cin >> inputString;
+
+    cout << "Enter any number of strings. Enter \"done\" to stop" << endl;
+    while(cin >> inputString && inputString != "done"){
         userInputtedStrings.push_back(inputString);
     }
-    //delete done from vector
-    userInputtedStrings.pop_back();
-    cout << "User inputted string were"<< endl;
-    for(int i=0; i < userInputtedStrings.size(); i++){
+
+    cout << endl << "User inputted string were:"<< endl;
+    for(int i = 0; i < userInputtedStrings.size(); i++){
         cout << userInputtedStrings.at(i) << ", ";
     }
 
-
-    cout<< endl << "The longest string/s are/is: "<< endl;
-    //call longeststring method
+    cout << endl << endl << "The longest string/s are/is: "<< endl;
     longestStringArray = returnLongestString(userInputtedStrings);
-    //print out strings
-    for(int i=0; i < longestStringArray.size(); i++){
+    for(int i = 0; i < longestStringArray.size(); i++){
         cout << longestStringArray.at(i) << ", ";
     }
-
     return 0;
 }
