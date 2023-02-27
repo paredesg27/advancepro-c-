f36eb584279a0e05a9d92d09f7fab5872541e561
GeorgeParedes <georgeparedes1234@gmail.com>
2023-02-27 14:16:14 -0500

    updated clasesd

---
+++ Grade.cpp
@@ -0,0 +1,5 @@
+//
+// Created by George on 2/25/2023.
+//
+#include "Grade.h"
+
---
+++ Logger.cpp
@@ -0,0 +1,10 @@
+//
+// Created by George on 2/25/2023.
+//
+
+#include "Logger.h"
+
+#include <iostream>
+
+using namespace std;
+
