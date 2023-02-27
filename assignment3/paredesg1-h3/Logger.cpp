243c9b8753be56cb05d3bf93813e14cdf1048865
George Paredes <119899081+paredesg27@users.noreply.github.com>
2023-02-27 14:23:46 -0500

    Update Logger.cpp

--- assignment3/paredesg1-h3/Logger.cpp
+++ assignment3/paredesg1-h3/Logger.cpp
@@ -1,7 +1,32 @@
 //
-// Created by Georg on 2/22/2023.
+// Created by George on 2/25/2023.
 //
-#include "string"
-class Logger {
 
+#ifndef PAREDESG1_H3_LOGGER_H
+#define PAREDESG1_H3_LOGGER_H
+#include <iostream>
+
+using namespace std;
+
+class Logger {
+public:
+    static Logger &instance() {
+        static Logger logger;
+        return logger;
+    }
+
+    static void log(const string &message) {
+
+        cout << message << endl;
+    }
+
+    Logger(const Logger &) = delete;
+
+    Logger &operator=(const Logger &) = delete;
+
+
+    Logger() = default;
 };
+
+#endif //PAREDESG1_H3_LOGGER_H
+
