62dd3e2ba252f4bbc6eaa8a8ee6aed46240f7298
George Paredes <georgeparedes1234@gmail.com>
2023-04-24 14:57:52 -0400

    print triage report done

--- HospitalAdmin.cpp
+++ HospitalAdmin.cpp
@@ -3,3 +3,23 @@
 //
 
 #include "HospitalAdmin.h"
+void HospitalAdmin::printTriageReport(const vector<Patient> &triageVector) {
+    string ailements = "{";
+    Logger::instance().log("Triage Report:\n");
+    if (triageVector.empty()) {
+        Logger::instance().log("No patients in triage\n");
+        return;
+    }
+
+    for (const auto& patient : triageVector) {
+        for (const auto& ailment: patient.ailments) {
+            ailements = ailment + ", ";
+        }
+        ailements += "}";
+
+        Logger::instance().log("Name:" + patient.firstName
+        + "\n Priority: " + to_string(patient.priority)
+        + "\n Symptoms: " + ailements);
+
+    }
+}
--- cmake-build-debug/CMakeFiles/HospitalAdmin.dir/HospitalAdmin.cpp.obj.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/HospitalAdmin.dir/HospitalAdmin.cpp.obj: \
- C:\Users\Georg\CLionProjects\advancepro-c-\HospitalAdmin.cpp \
- C:\Users\Georg\CLionProjects\advancepro-c-\HospitalAdmin.h
